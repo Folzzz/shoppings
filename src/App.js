590111871e766b5c968e6f3cbbaef214b735b273
kennyinspire <adunola.adeyemi@gmail.com>
2020-11-24 22:50:17 +0100

    product outlay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// feature 1
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Products from './components/Products';
 
 function App() {
+const [sort, setSort] = useState('')
+const [size, setSize] = useState('')
+const [isLoading, setIsLoading] = useState(true)
+const [products, setProducts] = useState([])
+
+const endPoint = `https://fakestoreapi.com/products`
+
+const getProductData = async() => {
+  const response = await fetch(endPoint)
+  const data = await response.json()
+  console.log(data);
+  setProducts(data);
+}
+
+useEffect (() => {
+  getProductData()
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href='/'>FX shopping</a>
       </header>
+
+      <main>
+        <div className="content">
+          <div className="main-bar">
+            <Products products={products}/>
+          </div>
+          <div className="side-bar">
+            cart
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        all right reserved
+      </footer>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Products.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import currencyFormat from '../util';
+import './products.css';
+
+function Products({products}) {
+    return (
+        <div>
+          <ul className="products">
+            {products.map(product => (
+                <li key={product.id} className="product-list">
+                    <div className="product">
+                        <a href={`#${product.id}`}>
+                            <img src={product.image} alt={product.description}/>
+                            <p>
+                                {product.title}
+                            </p>
+                        </a>
+                        <div className="product-price">
+                            <div>
+                                {currencyFormat(product.price)}
+                            </div>
+                            <button className="button primary">
+                                Add To Cart
+                            </button>
+                        </div>
+                    </div>
+
+                </li>
+            ))}
+          </ul>  
+        </div>
+    )
+}
+
+export default Products
